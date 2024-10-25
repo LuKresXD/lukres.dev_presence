3c5b79ebf6313df4d5202158e53ef48617caa068
LuKres <lukres006@gmail.com>
2024-10-25 09:42:48 +0300

    Initial commit

---
+++ src/routes/PresenceRouter.ts
@@ -0,0 +1,28 @@
+import Elysia from "elysia";
+import { client } from "..";
+import { sendPresence } from "../utils/PresenceUtils";
+
+let socket_map = new Map<string, string>()
+
+export const PresenceRouter = new Elysia({ prefix: "/presence" })
+    .ws('/', {
+        async open(ws) {
+            ws.subscribe(process.env.USER_ID!)
+            socket_map.set(ws.id, process.env.USER_ID!)
+            let guildMember = client.guilds.cache.get(process.env.GUILD_ID).members.cache.get(process.env.USER_ID!)
+            let newPresence = guildMember.presence
+            sendPresence(guildMember, newPresence, ws, true)
+        },
+        async message(ws, message) {
+            if (message === "ping") {
+                ws.send("pong")
+            }
+        },
+        async close(ws) {
+            if (socket_map.has(ws.id)) {
+                let discord_id = socket_map.get(ws.id)!
+                ws.unsubscribe(discord_id)
+                socket_map.delete(ws.id)
+            }
+        },
+    })
